6db66847adf6a4320712cd2fb16ff4b50d5e327f
chhhris <mr.lake@gmail.com>
2013-06-24 22:01:02 -0400

    student-sinatra initial commit

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'sinatra', require: 'sinatra/base'
+gem 'sqlite3'
+gem 'nokogiri'
+gem 'open-uri'
--- app.rb
+++ app.rb
@@ -1,10 +1,35 @@
 require 'sinatra/base'
+require_relative './lib/student.rb'
+require_relative './lib/persistable.rb'
 
 # Why is it a good idea to wrap our App class in a module?
 module StudentSite
+  
   class App < Sinatra::Base
     get '/' do
-      "hello world!"
+      erb :index
     end
+
+    get '/hello-world' do
+      @random_numbers = (1..20).to_a
+      erb :hello
+    end
+
+    get '/artists' do
+      @artists = ["John", "Paul", "Ringo", "George"]
+      erb :artists
+    end
+
+    get '/student_list' do
+      @students = Student.all
+      erb :student_list
+    end
+
+    get '/students/:id' do
+      @student = Student.find(params[:id])
+      erb :student_page
+    end
+
   end
+
 end
